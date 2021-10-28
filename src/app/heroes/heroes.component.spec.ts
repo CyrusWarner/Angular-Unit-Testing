d9bcd5de248980d56c08941f3d45675fa4e98bd7
CyrusWarner <cyruswarner27@gmail.com>
2021-10-28 16:44:20 -0500

    Added shallow integration test for the heroes component.

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,4 +1,15 @@
+import {
+  Component,
+  DebugElement,
+  Input,
+  NO_ERRORS_SCHEMA,
+  Output,
+} from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { of } from "rxjs";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component";
 describe("HeroesComponent", () => {
   let component: HeroesComponent;
@@ -37,5 +48,52 @@ describe("HeroesComponent", () => {
 
       expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
     });
+  });
+});
+
+describe("HeroesComponent (shallow tests)", () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let component: HeroesComponent;
+  let debugEl: DebugElement;
+  let mockHeroService;
+  let HEROES;
+
+  @Component({
+    selector: "app-hero",
+    template: "<div></div>",
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+  }
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: "SpiderDude", strength: 8 },
+      { id: 2, name: "Wonderful Woman", strength: 28 },
+      { id: 3, name: "SuperDude", strength: 28 },
+    ];
+    mockHeroService = jasmine.createSpyObj([
+      "getHeroes",
+      "addHero",
+      "deleteHero",
+    ]);
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent, FakeHeroComponent],
+      providers: [{ provide: HeroService, useValue: mockHeroService }],
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+  });
+  it("should set heroes correctly from the hero service", () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(component.heroes.length).toBe(3);
+  });
+  it("should create one li for each hero", () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(debugEl.queryAll(By.css("li")).length).toBe(3);
   });
 });
